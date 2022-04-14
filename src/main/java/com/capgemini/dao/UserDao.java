1e7e8bf35f357df2a305ea929c5d04bf3a389024
Sumeet Balwade <balwadesr@gmail.com>
2022-04-14 21:20:55 +0530

    password not valid

--- src/main/java/com/capgemini/dao/UserDao.java
+++ src/main/java/com/capgemini/dao/UserDao.java
@@ -1,6 +1,8 @@
 package com.capgemini.dao;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,48 +19,77 @@ import com.capgemini.repository.UserBasicRepository;
 import com.capgemini.service.UserService;
 
 @Service
-public class UserDao implements UserService{
-	
+public class UserDao implements UserService {
+
 	@Autowired
 	UserBasicRepository ubrepo;
 
 	@Autowired
 	UserAdvancedRepository uadrepo;
-	
+
+	public static boolean isValidPassword(String password) {
+
+		// Regex to check valid password.
+		String regex = "^(?=.*[0-9])" + "(?=.*[a-z])(?=.*[A-Z])" + "(?=.*[@#$%^&+=])" + "(?=\\S+$).{8,20}$";
+
+		// Compile the ReGex
+		Pattern p = Pattern.compile(regex);
+
+		// If the password is empty
+		// return false
+		if (password == null) {
+			return false;
+		}
+
+		// Pattern class contains matcher() method
+		// to find matching between given password
+		// and regular expression.
+		Matcher m = p.matcher(password);
+
+		// Return if the password
+		// matched the ReGex
+		return m.matches();
+	}
+
 	@Override
 	public void UserRegisterService(UserBasic userbasic) throws UserAlreadyExistsException {
-		if(ubrepo.existsById(userbasic.getEmail())) {
+		if (ubrepo.existsById(userbasic.getEmail())) {
 			throw new UserAlreadyExistsException("The User already exists.");
 		}
-		ubrepo.save(userbasic);
+		if(isValidPassword(userbasic.getPassword())) {
+			
+			ubrepo.save(userbasic);
+		}else {
+			throw new UserAlreadyExistsException("Passsword is not valid");
+		}
+
 	}
 
-	
 	@Override
 	public void applyVehicleLoan(LoanAppTable loanapp, UserBasic userbasic) {
-		
+
 	}
 
 	@Override
-	public void resetPasswordService(UserBasic userbasic) throws UserNotFoundException{
-		if(!ubrepo.existsById(userbasic.getEmail())) {
+	public void resetPasswordService(UserBasic userbasic) throws UserNotFoundException {
+		if (!ubrepo.existsById(userbasic.getEmail())) {
 			throw new UserNotFoundException("The User is not found.");
 		}
-		UserBasic ub=ubrepo.getById(userbasic.getEmail());
+		UserBasic ub = ubrepo.getById(userbasic.getEmail());
 		ub.setPassword(userbasic.getPassword());
 		ubrepo.save(ub);
 	}
 
 	@Override
 	public void modifyUserDetails(UserAdvanced user) {
-		if(uadrepo.existsById(user.getUserId())) {
-			
+		if (uadrepo.existsById(user.getUserId())) {
+
 		}
 	}
 
 	@Override
 	public UserBasic getUserRegistrationdetails(String email) throws UserNotFoundException {
-		if(!ubrepo.existsById(email)){
+		if (!ubrepo.existsById(email)) {
 			throw new UserNotFoundException("The User is not found.");
 		}
 		return ubrepo.getById(email);
@@ -96,11 +127,11 @@ public class UserDao implements UserService{
 
 	@Override
 	public boolean verifyUserLogin(LoginDto login) throws UserNotFoundException {
-		if(!ubrepo.existsById(login.getEmail())) {
+		if (!ubrepo.existsById(login.getEmail())) {
 			throw new UserNotFoundException("The User is not found.");
 		}
-		UserBasic ub=ubrepo.getById(login.getEmail());
-		if(ub.getEmail().equals(login.getEmail()) && ub.getPassword().equals(login.getPassword()))
+		UserBasic ub = ubrepo.getById(login.getEmail());
+		if (ub.getEmail().equals(login.getEmail()) && ub.getPassword().equals(login.getPassword()))
 			return true;
 		return false;
 	}
